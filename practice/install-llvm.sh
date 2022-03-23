16a82a735b855e524f35e27fb88cd5907a0ff107
strikef <seunghyeon.nam@sf.snu.ac.kr>
2022-03-23 17:46:27 +0900

    Update LLVM build script

--- practice/install-llvm.sh
+++ practice/install-llvm.sh
@@ -3,26 +3,30 @@
 # Install necessary dependencies
 # Package names may slightly differ in macOS (brew)
 sudo apt update
-sudo apt -y install git curl g++ cmake ninja-build python3-distutils zlib1g-dev \
+sudo apt -y install git g++ cmake ninja-build python3-distutils zlib1g-dev \
 libncurses-dev
 
 # Download LLVM source
-curl -OL https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.1/llvm-project-13.0.1.src.tar.xz
-tar -xf llvm-project-13.0.1.src.tar.xz
-cd llvm-project-13.0.1.src
+git clone -b llvmorg-14.0.0 https://github.com/llvm/llvm-project.git --depth 1
+cd llvm-project
 
 # Create LLVM installation directory
-LLVM_DIR=/opt/llvm # Edit this directory
+LLVM_DIR=/opt/llvm-swpp # Edit this directory
 sudo mkdir $LLVM_DIR
 
 # Build LLVM
 # M1 mac users should use AArch64 instead of X86
 cmake -G Ninja -S llvm -B build \
     -DLLVM_ENABLE_PROJECTS="clang;lldb" \
+    -DLLVM_INSTALL_UTILS=ON \
     -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DLLVM_ENABLE_RTTI=ON \
+    -DLLVM_ENABLE_EH=ON \
     -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    '-DCMAKE_JOB_POOLS:STRING=link=1' \
-    -DCMAKE_JOB_POOL_LINK:STRING=link \
+    -DLLVM_PARALLEL_COMPILE_JOBS=4 \
+    -DLLVM_PARALLEL_LINK_JOBS=1 \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$LLVM_DIR
-sudo cmake --build build --target install
+cmake --build build
+sudo cmake --install build
